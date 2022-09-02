e7ec5e22aca91f54c08fb65dc76f8ce02e8a29d6
Akhil Kammila <akhilkammila@gmail.com>
2022-09-01 20:35:02 -0400

    check if there are changes

--- src/BarContainer.js
+++ src/BarContainer.js
@@ -1,66 +1,66 @@
 import { Bar } from 'react-chartjs-2';
 
-function BarContainer({data}) {
-  console.log('barcontainer is re-rendering')
-  console.log(data)
+function BarContainer({ data }) {
+    console.log('barcontainer is re-rendering')
+    console.log(data)
 
-  const options = {
-    scales:{  
-        y:{
-            grid:{
-                lineWidth:1,
-                color: "#000000",
-                z: -1
-            },
-            ticks:{
-                font:{
-                    size: 15,
+    const options = {
+        scales: {
+            y: {
+                grid: {
+                    lineWidth: 1,
+                    color: "#000000",
+                    z: -1
                 },
-                color: "#000000"
+                ticks: {
+                    font: {
+                        size: 15,
+                    },
+                    color: "#000000"
+                }
+            },
+            x: {
+                min: -2,
+                max: 6,
+                grid: {
+                    lineWidth: 1,
+                    color: "#000000"
+                },
+                ticks: {
+                    font: {
+                        size: 20,
+                    },
+                    color: "#000000"
+                }
             }
         },
-        x:{
-            min:-6,
-            max:43,
-            grid:{
-                lineWidth:1,
-                color: "#000000"
+        indexAxis: 'y',
+        elements: {
+            bar: {
+                borderWidth: 2,
             },
-            ticks:{
-                font:{
-                    size: 20,
+        },
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top",
+                align: "end"
+            },
+            title: {
+                display: true,
+                font: {
+                    size: 40
                 },
+                text: 'Crop Yield Analysis',
                 color: "#000000"
-            }
-        }
-    },
-    indexAxis: 'y',
-    elements: {
-        bar: {
-        borderWidth: 2,
-        },
-    },
-    responsive: true,
-    plugins: {
-        legend: {
-            position: "top",
-            align: "end"
-        },
-        title: {
-            display: true,
-            font:{
-                size: 40
             },
-            text: 'Crop Yield Analysis',
-            color: "#000000"
         },
-    },
 
-  };
+    };
 
-  return (
-    <Bar key={data} options={options} data={data}/>
-  )
+    return (
+        <Bar key={data} options={options} data={data} />
+    )
 
 }
 
